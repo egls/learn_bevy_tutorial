437af79cc0d2eb02b5d46ee597d7f8075105caf0
stefan <stefan.egli@gmail.com>
2023-03-30 21:17:18 +0200

    fixed z coord issue, set camera to high z value

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use bevy::window::PrimaryWindow;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
+        .add_startup_system(spawn_camera)
+        .add_system(player_movement)
+        .add_system(confine_player_movement)
         .run();
 }
+
+pub const PLAYER_SPEED: f32 = 25.0;
+pub const PLAYER_SIZE: f32 = 64.0;
 
 #[derive(Component)]
 pub struct Player {}
@@ -32,9 +37,91 @@ pub fn spawn_player(
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
 
+    //commands.spawn(Camera2dBundle::default());
+    println!("width: {}", window.width() / 2.0);
+
+    // the camera needs to be placed at a high z coord if transformed "by hand";
+    // ::default() should set Z to +999
     commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 100.0),
         ..default()
     });
+}
 
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        // normailze direction
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * PLAYER_SPEED + time.delta_seconds();
+        // move independent of frame rate
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = PLAYER_SIZE / 2.0;
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_transform.translation;
+
+        // Wrap the player's position around the window borders
+        //if translation.x < -window.width() / 2.0 {
+        //    translation.x = window.width() / 2.0;
+        //} else if translation.x > window.width() / 2.0 {
+        //    translation.x = -window.width() / 2.0;
+        //}
+
+        //if translation.y < -window.height() / 2.0 {
+        //    translation.y = window.height() / 2.0;
+        //} else if translation.y > window.height() / 2.0 {
+        //    translation.y = -window.height() / 2.0;
+        //}
+
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player_transform.translation = translation;
+    }
 }
