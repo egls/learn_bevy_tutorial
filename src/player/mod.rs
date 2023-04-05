ebe90b466cf4261b6396f506c8b3c810f9e796e6
stefan <stefan.egli@gmail.com>
2023-04-05 19:17:05 +0200

    episode 8 impl. partially

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -5,12 +5,30 @@ mod systems;
 
 use systems::*;
 
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystem;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_system(player_movement)
+        app
+        //.configure_set(MovementSystemSet.before(ConfinementSystem))
+        .add_startup_system(spawn_player)
+        .add_system(player_movement)
+        .add_system(player_movement.before(confine_player_movement))
+            //.add_systems(
+            //    (
+            //        player_movement,
+            //        confine_player_movement
+            //    )
+            // ).chain()
+            //.add_system(confine_player_movement.after(player_movement))
             .add_system(confine_player_movement)
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
