bc0c007c0e3cc24e8490cbd7cc982e1412b84c8a
stefan <stefan.egli@gmail.com>
2023-04-09 21:09:32 +0200

    refactor, optimizations from episode 09

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -23,19 +23,29 @@ impl Plugin for PlayerPlugin {
             .configure_set(MovementSystemSet.before(ConfinementSystemSet))
             // On Enter State
             .add_system(spawn_player.in_schedule(OnEnter(AppState::Game)))
+            // Systems New
+            .add_systems(
+                (
+                    player_movement.in_set(MovementSystemSet),
+                    confine_player_movement.in_set(ConfinementSystemSet),
+                )
+                    .in_set(OnUpdate(AppState::Game))
+                    .in_set(OnUpdate(SimulationState::Running)),
+            )
+            // Systems OLD
             // Systems
-            .add_system(
-                player_movement
-                    .in_set(MovementSystemSet)
-                    .run_if(in_state(AppState::Game))
-                    .run_if(in_state(SimulationState::Running)),
-            )
-            .add_system(
-                confine_player_movement
-                    .in_set(ConfinementSystemSet)
-                    .run_if(in_state(AppState::Game))
-                    .run_if(in_state(SimulationState::Running)),
-            )
+            //.add_system(
+            //    player_movement
+            //        .in_set(MovementSystemSet)
+            //        .run_if(in_state(AppState::Game))
+            //        .run_if(in_state(SimulationState::Running)),
+            // )
+            //.add_system(
+            //    confine_player_movement
+            //        .in_set(ConfinementSystemSet)
+            //        .run_if(in_state(AppState::Game))
+            //        .run_if(in_state(SimulationState::Running)),
+            //)
             .add_systems(
                 (enemy_hit_player, player_hit_star)
                     .in_set(OnUpdate(AppState::Game))
