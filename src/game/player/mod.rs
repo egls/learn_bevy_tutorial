542cc6267f7737672053dd1295cd1f8ac31c69f1
stefan <stefan.egli@gmail.com>
2023-04-09 21:10:51 +0200

    cleaup

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -23,7 +23,6 @@ impl Plugin for PlayerPlugin {
             .configure_set(MovementSystemSet.before(ConfinementSystemSet))
             // On Enter State
             .add_system(spawn_player.in_schedule(OnEnter(AppState::Game)))
-            // Systems New
             .add_systems(
                 (
                     player_movement.in_set(MovementSystemSet),
@@ -32,20 +31,6 @@ impl Plugin for PlayerPlugin {
                     .in_set(OnUpdate(AppState::Game))
                     .in_set(OnUpdate(SimulationState::Running)),
             )
-            // Systems OLD
-            // Systems
-            //.add_system(
-            //    player_movement
-            //        .in_set(MovementSystemSet)
-            //        .run_if(in_state(AppState::Game))
-            //        .run_if(in_state(SimulationState::Running)),
-            // )
-            //.add_system(
-            //    confine_player_movement
-            //        .in_set(ConfinementSystemSet)
-            //        .run_if(in_state(AppState::Game))
-            //        .run_if(in_state(SimulationState::Running)),
-            //)
             .add_systems(
                 (enemy_hit_player, player_hit_star)
                     .in_set(OnUpdate(AppState::Game))
@@ -53,13 +38,5 @@ impl Plugin for PlayerPlugin {
             )
             // On Exit State
             .add_system(despawn_player.in_schedule(OnExit(AppState::Game)));
-
-        //.add_systems(
-        //    (
-        //        player_movement,
-        //        confine_player_movement
-        //    )
-        // ).chain()
-        //.add_system(confine_player_movement.after(player_movement))
     }
 }
