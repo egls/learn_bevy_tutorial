d029dec5096e5a331ee461e8698978dcee069cfc
stefan <stefan.egli@gmail.com>
2023-04-09 21:15:06 +0200

    add pause, resume simulation

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -21,19 +21,23 @@ impl Plugin for GamePlugin {
         app.add_state::<SimulationState>()
             // Events
             .add_event::<GameOver>()
+            // On Enter Systems
+            .add_system(pause_simulation.in_schedule(OnEnter(AppState::Game)))
             // Plugin
             .add_plugin(EnemyPlugin)
             .add_plugin(PlayerPlugin)
             .add_plugin(ScorepPlugin)
             .add_plugin(StarPlugin)
             // Systems
-            .add_system(toggle_simulation.run_if(in_state(AppState::Game)));
+            .add_system(toggle_simulation.run_if(in_state(AppState::Game)))
+            // On Exit System
+            .add_system(resume_simulation.in_schedule(OnExit(AppState::Game)));
     }
 }
 
 #[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
 pub enum SimulationState {
-    Running,
     #[default]
+    Running,
     Paused,
 }
